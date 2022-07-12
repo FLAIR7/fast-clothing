f7b290696f2926bc11f0096faad4fdf22d6a97f2
Vini Sousa <guilty77@tutanota.com>
2022-07-12 15:40:52 -0300

    fix: frontend

--- src/frontend/src/services/auth.js
+++
@@ -1,8 +0,0 @@
-export function getAuthHeader() {
-    const token = localStorage.getItem("token");
-    if(token){
-        return {'Authorization': 'Bearer ' + token};
-    } else {
-        return {};
-    }
-}
